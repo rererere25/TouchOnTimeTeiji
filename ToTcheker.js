6750a1d6b4ff282cce54f3ee0d5a6b158e4521b9
Vale <valerauko@gmail.com>
2020-02-07 19:51:37 +0900

    Fix typo
    remining -> remaining

--- ToTcheker.js
+++ ToTcheker.js
@@ -11,12 +11,12 @@ javascript:(function() {
     .querySelector("body > div > div.htBlock-mainContents > div > div.htBlock-autoNewLineTable.specific-autoNewLineTable > ul > li:nth-child(1) > div")
     .innerText;
 
-  let reminingDays = allWorkDays - currentWorkDays;
-  let reminingHours = totalHours - currentHours;
-  let averageWorkHours = reminingHours / reminingDays;
+  let remainingDays = allWorkDays - currentWorkDays;
+  let remainingHours = totalHours - currentHours;
+  let averageWorkHours = remainingHours / remainingDays;
 
   alert('今月の残労働基準時間は'
-    + reminingHours.toFixed(2)
+    + remainingHours.toFixed(2)
     + '時間です。残りの勤務日で平均'
     + averageWorkHours.toFixed(2)
     + '時間働けば到達するよ！！残業時間±0で元気に月末を迎えような？？？');
